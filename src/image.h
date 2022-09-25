7cb9717afebe9cfd756f433e8ac097c2acf9d1ac
kinakomoti-321 <kinakomoti743@gmail.com>
2022-09-25 16:03:15 +0900

    Image class add

---
+++ src/image.h
@@ -0,0 +1,84 @@
+#ifndef _IMAGE_H
+#define _IMAGE_H
+
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "./vec3.h"
+
+class Image
+{
+private:
+    unsigned int width;
+    unsigned int height;
+    float *pixel;
+
+public:
+    Image()
+    {
+        width = 1;
+        height = 1;
+        pixel = new float[width * height * 3];
+    }
+
+    Image(unsigned int width, unsigned int height) : width(width), height(height)
+    {
+        pixel = new float[width * height * 3];
+    }
+
+    ~Image()
+    {
+        delete[] pixel;
+    }
+
+    unsigned int getWidth() const { return width; };
+    unsigned int getHeight() const { return height; };
+
+    void setPixel(unsigned int i, unsigned int j, const Vec3 &RGB)
+    {
+        unsigned int index = (i + j * width) * 3;
+        pixel[index] = RGB[0];
+        pixel[index + 1] = RGB[1];
+        pixel[index + 2] = RGB[2];
+    }
+
+    void writePPM(const std::string &filename) const
+    {
+        std::ofstream file(filename + ".ppm");
+        file << "P3" << std::endl;
+        file << width << " " << height << std::endl;
+        file << "255" << std::endl;
+
+        for (int j = 0; j < width; ++j)
+        {
+            for (int i = 0; i < width; ++i)
+            {
+                unsigned int index = (i + j * width) * 3;
+                Vec3 RGB(pixel[index], pixel[index + 1], pixel[index + 2]);
+                Vec3 G_RGB = gammaConver(RGB);
+                G_RGB = RGB;
+
+                const unsigned int R = static_cast<unsigned int>(255 * G_RGB[0]);
+                const unsigned int G = static_cast<unsigned int>(255 * G_RGB[1]);
+                const unsigned int B = static_cast<unsigned int>(255 * G_RGB[2]);
+
+                file << R << " " << G << " " << B << std::endl;
+            }
+        }
+
+        file.close();
+    }
+
+    Vec3 gammaConver(const Vec3 &RGB) const
+    {
+        Vec3 G_RGB;
+        G_RGB[0] = std::pow(RGB[0], 1.0f / 2.2f);
+        G_RGB[1] = std::pow(RGB[1], 1.0f / 2.2f);
+        G_RGB[2] = std::pow(RGB[2], 1.0f / 2.2f);
+
+        return G_RGB;
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <image.h>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    Image img(512, 512);
+    const unsigned int width = img.getWidth();
+    const unsigned int height = img.getHeight();
+
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            Vec3 RGB(static_cast<float>(i) / width, static_cast<float>(j) / height, 1.0f);
+            img.setPixel(i, j, RGB);
+        }
+    }
+
+    img.writePPM("test");
+
     return 0;
 }
